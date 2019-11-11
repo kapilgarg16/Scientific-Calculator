670431be77d08232aedddcdc32e0ee5cf1b9f9ab
kapil garg <55026503+kapilgarg16@users.noreply.github.com>
2019-11-12 04:57:02 +0530

    Add files via upload

---
+++ scientific calsy.py
@@ -0,0 +1,86 @@
+from tkinter import*
+from math import*
+w=Tk()
+w.title("My calculator")
+w.iconbitmap()
+w.config(bg="black")
+w.minsize(width=400,height=400)
+def display(x):
+    global s1
+    if x=="AC":
+        s1=''
+        s.set(s1)
+    elif x=="DEL":
+        s1=s1[:len(s.get())-1]
+        s.set(s1)
+    elif x!="=":
+        s1=s1+x
+        s.set(s1)
+    else:
+        try:
+            if s1[0]=="√":
+                s.set(str(sqrt(int(s.get()[1:]))))
+            elif s1[0:3] == "sin":
+                s.set(str(sin(int(s.get()[3:]))))
+            elif s1[0:3] == "cos":
+                s.set(str(cos(int(s.get()[3:]))))
+            elif s1[0:3] == "tan":
+                s.set(str(tan(int(s.get()[3:]))))
+            elif s1[0:2] in "ln":
+                s.set(str(log(float(s.get()[2:]))))
+            elif s1[0:3] in "log":
+                s.set(str(log10(float(s.get()[3:]))))
+            elif s1[0]=="!":
+                    s.set(str(factorial(int(s.get()[1:]))))
+            elif s1[len(s1)-1] == "!":
+                     s.set(str(factorial(int(s.get()[:len(s1)-1]))))
+            elif s1[0:4] == "cosh":
+                s.set(str(cosh(int(s.get()[4:]))))
+            elif s1[0:4] == "sinh":
+                s.set(str(sinh(int(s.get()[4:]))))
+            elif s1[0:4] == "tanh":
+                s.set(str(tanh(int(s.get()[4:]))))
+            elif s1[0:2]=="e ":
+                s.set(str(exp(int(s.get()[2:]))))
+            elif s1[len(s1)-1:] == "²":
+                s.set(str((int(s.get()[:len(s1)-1]))**2))
+            elif s1[len(s1)-2:]=="X3":
+                s.set(str((int(s.get()[:len(s1)-2]))**3))
+            else:
+                s.set(str(eval(s.get())))
+        except Exception as e:
+            s.set(e)
+            s1=''
+s1=''
+s=StringVar()
+fe=Frame(w,bg="#123456")
+e=Entry(fe,textvariable=s,font="arial 25 bold",justify=RIGHT)
+e.pack(padx=10,pady=10,expand=YES,fill=BOTH)
+fe.pack(padx=10,pady=10 ,expand=YES,fill=BOTH)
+fe=Frame(w,bg="#123456")
+for i in ["DEL","AC"]:
+    b = Button(fe, text=i, bg="black", fg="white", font="arial 15 bold",relief=RAISED,command =lambda x=i:display(x))
+    b.pack(side=LEFT, padx=5, pady=5, expand=YES, fill=BOTH)
+fe.pack(padx=10,pady=10 ,expand=YES,fill=BOTH)
+fe=Frame(w,bg="#123456")
+for i in ["sin","cos","tan","log","eⁿ"]:
+    b = Button(fe, text=i, bg="grey", fg="white", font="arial 10 bold",relief=RAISED,command=lambda x=i if i!="eⁿ" else "e ":display(x))
+    b.pack(side=LEFT, padx=4, pady=4, expand=YES, fill=BOTH)
+fe.pack(padx=10,pady=10 ,expand=YES,fill=BOTH)
+fe=Frame(w,bg="#123456")
+for i in ["sinh","cosh","tanh","X!","ln"]:
+    b = Button(fe, text=i, bg="grey", fg="white", font="arial 10 bold",relief=RAISED,command=lambda x=i if i!="X!"else "!":display(x))
+    b.pack(side=LEFT, padx=4, pady=4, expand=YES, fill=BOTH)
+fe.pack(padx=10,pady=10 ,expand=YES,fill=BOTH)
+fe=Frame(w,bg="#123456")
+for i in ["X²","X3","^","nCr","√"]:
+    b = Button(fe, text=i, bg="grey", fg="white", font="arial 10 bold",relief=RAISED,command=lambda x=i if i!="X²" else "²" :display(x))
+    b.pack(side=LEFT, padx=4, pady=4, expand=YES, fill=BOTH)
+fe.pack(padx=10,pady=10 ,expand=YES,fill=BOTH)
+for i in ["789/","456*","123+",".0=-"]:
+    f=Frame(w,bg="black")
+    for j in i:
+        b=Button(f,text=j,bg="black",fg="white",font="arial 15 bold",relief=RAISED,command=lambda x=j:display(x))
+        b.pack(side=LEFT,padx=10,pady=10,expand=YES,fill=BOTH)
+    f.pack(padx=10,pady=10,expand=YES,fill=BOTH)
+w.mainloop()
